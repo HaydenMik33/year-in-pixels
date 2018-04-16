9a36ac4dafb34145c738cbd74a6c28a80e9cdb54
HaydenMik33 <36651485+HaydenMik33@users.noreply.github.com>
2018-04-16 18:41:44 -0500

    Update Welcome.js

--- src/components/public/Welcome/Welcome.js
+++ src/components/public/Welcome/Welcome.js
@@ -1,21 +1,23 @@
 import React, { Component } from "react";
 import "./Welcome.css";
 import { Link } from "react-router-dom";
+import Blocks from "./Blocks";
 class Welcome extends Component {
   render() {
     return (
-      <div>
-        <div className="Intro_content_box">
+      <div className="welcome">
+        <Blocks />
+        <div className="welcome_content">
           <h1 className="welcome_Ilgi">Ilgi</h1>
-          <span className="letter">일기</span>
-          <span className="letter">日记</span>
-          <span className="letter">ダイアリー</span>
+          <span className="welcome_letter">일기</span>
+          <span className="welcome_letter">日记</span>
+          <span className="welcome_letter">ダイアリー</span>
           <p>
             COLOR YOUR YEAR IN <br />
-            <span>PIXELS</span>
+            <span className="">PIXELS</span>
           </p>
           <a href={process.env.REACT_APP_LOGIN}>
-            <button>GET STARTED</button>
+            <button className="welcome_button">GET STARTED</button>
           </a>
         </div>
       </div>
