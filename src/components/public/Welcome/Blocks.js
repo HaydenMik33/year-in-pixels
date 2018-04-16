b5f63b300ed5df5c2bf4028163ff27470e09f9d6
HaydenMik33 <36651485+HaydenMik33@users.noreply.github.com>
2018-04-16 18:44:03 -0500

    Create Blocks.js

---
+++ src/components/public/Welcome/Blocks.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./Blocks.css";
+const Blocks = () => {
+  return (
+    <div className="blocks">
+      <div className="blocks_block" />
+      <div className="blocks_block" />
+      <div className="blocks_block" />
+      <div className="blocks_block" />
+      <div className="blocks_block" />
+      <div className="blocks_block" />
+      <div className="blocks_block" />
+      <div className="blocks_block" />
+    </div>
+  );
+};
+
+export default Blocks;
